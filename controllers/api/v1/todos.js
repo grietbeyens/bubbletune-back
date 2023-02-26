e705788e59badb790be4c0199cd2b66c40de16a6
Griet Beyens <beyensgriet@gmail.com>
2023-02-26 20:08:38 +0100

    feat: connection with mongodb (mongoose)

--- controllers/api/v1/todos.js
+++ controllers/api/v1/todos.js
@@ -1,17 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const todoSchema = new Schema({
+  text: String,
+  user: String,
+  completed: Boolean,
+});
+const Todo = mongoose.model("Todo", todoSchema);
+
 const getAll = (req, res) => {
-  res.send("GET todos");
+  /* res.send("GET todos"); */
+  res.json({
+    status: "succes",
+    data: {
+      todos: [],
+    },
+  });
 };
 
 const create = (req, res) => {
-  res.send("POST todos");
+  let todo = new Todo();
+  todo.text = "My first todo";
+  todo.user = "Griet";
+  todo.completed = false;
+  todo.save((err, doc) => {
+    if (!err) {
+      res.json({
+        status: "succes",
+        data: {
+          todo: doc,
+        },
+      });
+    }
+  });
 };
 
 const update = (req, res) => {
-  res.send("PUT todos");
+  /* res.send("PUT todos"); */
+  res.json({
+    status: "succes",
+    data: {
+      text: "UPDATED todo with id ",
+    },
+  });
 };
 
 const remove = (req, res) => {
-  res.send("DELETE todo " + req.params.id);
+  /* res.send("DELETE todo " + req.params.id); */
+  res.json({
+    status: "succes",
+    data: {
+      text: "REMOVED todo with id ",
+    },
+  });
 };
 
 module.exports.getAll = getAll;
