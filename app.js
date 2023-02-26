582fe9b02aea497e6dbd888b4ca52e7590da1ae2
Griet Beyens <beyensgriet@gmail.com>
2023-02-26 17:42:55 +0100

    refactor: structure routers

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const apiV1TodosRouter = require("./routers/api/v1/todos");
 
 app.get("/", (req, res) => res.send("Hello World"));
-
-app.get("/api/v1/todos", (req, res) => {
-  res.send("GET todos");
-});
-
-app.post("/api/v1/todos", (req, res) => {
-  res.send("POST todos");
-});
-
-app.put("/api/v1/todos/:id", (req, res) => {
-  res.send("PUT todos");
-});
-
-app.delete("/api/v1/todos/:id", (req, res) => {
-  res.send("DELETE todo " + req.params.id);
-});
+app.use("/api/v1/todos", apiV1TodosRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
