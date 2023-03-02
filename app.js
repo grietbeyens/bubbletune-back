a3d2f9b4d20cc83c292bd03b8da26b0bdff3ae25
Griet Beyens <beyensgriet@gmail.com>
2023-03-02 14:23:15 +0100

    feat: add model with Schema

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/todosapp");
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 const apiV1TodosRouter = require("./routers/api/v1/todos");
 
