5241ed63a658af4cb807c533ae9840c232916562
Griet Beyens <beyensgriet@gmail.com>
2023-03-05 14:01:45 +0100

    feat: add cors dependency

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/todosapp");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const port = 3000;
 const apiV1TodosRouter = require("./routers/api/v1/todos");
